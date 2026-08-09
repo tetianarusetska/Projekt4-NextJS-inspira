2f30c279c6f6b2db02bc0d88910d2098dda691c3
Rusetska Tetiana <rusetskatata@gmail.com>
2026-08-09 17:37:59 +0200

    hero content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import Header from "./s_components/Header";
 import Background from "./s_components/Background";
+import HeroContent from "./s_components/HeroContent";
 
 export default function Home() {
   return (
     <div className="relative h-screen w-screen">
       <Background />
       <Header />
+      <HeroContent />
     </div>
   );
 }
--- src/app/s_components/HeroContent.tsx
+++ src/app/s_components/HeroContent.tsx
@@ -1,10 +1,67 @@
 export default function HeroContent() {
 
-  return (
-    <>
-    <div>
-        
-    </div>
-    </>
-  );
+    return (
+        <>
+            <div className="relative z-10">
+                <p
+                    className="absolute left-10.75 top-35 text-[96px] leading-25.5 tracking-[1.0px]"
+                >
+                    Mehr als ein<br></br>Archiv
+                </p>
+                <p
+                    className="absolute left-78 top-75 text-[32px] leading-25 tracking-[0.5px]"
+                >
+                    Jede Idee beginnt mit einer Inspiration
+                </p>
+            </div>
+
+            <div className="relative z-10">
+                <p
+                    className="text-right grotesk-xbold absolute left-295 top-6 text-[48px] leading-9.5 tracking-normal uppercase"
+                >
+                    Dein<br></br>personlicher<br></br>Sammlungsraum
+                </p>
+            </div>
+
+            <div className="relative z-10">
+                <div>
+                    <p
+                        className="text-right grotesk-xbold absolute left-266 top-63.5 text-[96px] leading-9.5 tracking-normal uppercase"
+                    >
+                        10
+                    </p>
+                    <p
+                        className="text-right grotesk-xbold absolute left-250 top-81.75 text-[48px] leading-9.5 tracking-normal uppercase"
+                    >
+                        Sammlungen
+                    </p>
+                </div>
+                <div>
+                    <p
+                        className="text-right grotesk-xbold absolute left-315 top-63.5 text-[96px] leading-9.5 tracking-normal uppercase"
+                    >
+                        11272
+                    </p>
+                    <p
+                        className="text-right grotesk-xbold absolute left-318 top-81.75 text-[48px] leading-9.5 tracking-normal uppercase"
+                    >
+                        Objekte
+                    </p>
+                </div>
+            </div>
+
+            <div className="relative z-10">
+                <button
+                    className="uppercase absolute top-148 left-150 text-center grotesk-xbold text-[24px] tracking-[1px] w-48.75 h-12.25 bg-(--mainColor) text-white"
+                >
+                    Sammlungen
+                </button>
+                <button
+                    className="uppercase absolute top-148 left-250  text-center grotesk-xbold text-[24px] w-48.75 h-12.25 tracking-[1px] border-2 border-(--mainColor)"
+                >
+                    Archivieren
+                </button>
+            </div>
+        </>
+    );
 }
