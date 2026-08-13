84bcc3c8dea81777cc75f8138563ad4bd69b13a6
Rusetska Tetiana <rusetskatata@gmail.com>
2026-08-13 19:37:18 +0200

    archive: + start productPage

--- src/app/archive/Product.tsx
+++ src/app/archive/Product.tsx
@@ -3,6 +3,25 @@
 
 export default function Product() {
     return (
-        <div></div>
+        <div id="product" className="mb-20">
+
+            <div className="mt-20 ml-5.25 flex flex-col gap-4">
+                <p className="grotesk-xbold text-[32px] leading-[100%] tracking-[1%]">
+                    04    –    VORSCHAU
+                </p>
+
+                <p className="grotesk-xbold text-[64px] leading-[84%]">
+                    ein letzter blick.
+                </p>
+
+                <p className="text-[24px] leading-[100%]">
+                    Uberprufe deine Angaben. Dann ist<br></br>dein Objekt bereit fur dein Archiv.
+                </p>
+            </div>
+
+            
+            <div></div>
+
+        </div>
     )
 }
