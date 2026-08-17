88ed1bf8d2dd4c4bcb2849286cd3f549751d7f37
Rusetska Tetiana <rusetskatata@gmail.com>
2026-08-17 14:22:41 +0200

    fix

---
+++ prisma/client.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool } from "pg";
+
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+const isVercel = !!process.env.VERCEL;
+
+const adapter = isVercel
+  ? new PrismaNeon({ connectionString: process.env.DATABASE_URL! })
+  : new PrismaPg(new Pool({ connectionString: process.env.DATABASE_URL }));
+
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
